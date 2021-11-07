50a9e970432a8dd9845d5ef936c3e5cf95cd158a
batelzag <88581982+batelzag@users.noreply.github.com>
2021-11-07 12:29:16 +0200

    Add files via upload

---
+++ Homerwork3/Setups/Environments/PROD/variables.tf
@@ -0,0 +1,12 @@
+variable "enviroment_tag" {
+    description = "which enviroment is it"
+    default = "Prodaction"
+}
+variable "owner_tag" {
+    description = "who's the owner of the project"
+    default = ""
+}
+variable "project_tag" {
+    description = "what is the project about"
+    default = ""
+}
