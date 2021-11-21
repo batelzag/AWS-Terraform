2e582f75c7d148ca6020a00cf5d9ff802bf6cb35
batelzag <88581982+batelzag@users.noreply.github.com>
2021-11-21 13:34:58 +0200

    Delete Homework4 directory

--- Homework4/modules/web/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Output the private ip of webserver for further use on other modules
-output "web_privateip" {
-    value = "${aws_instance.web.*.private_ip}"
-    description = "the private ip of web servers"
-}
--- Homework4/modules/web/s3bucket.tf
+++
@@ -1,9 +0,0 @@
-# Creates a S3 bucket for uploading nginx access logs
-resource "aws_s3_bucket" "s3-bucket" {
-   bucket = "${var.bucket_name}"
-   acl    = "private"
-
-   tags = {
-     Name        = "${var.bucket_name}"
-   }
-}
